5bd9beafa2097b3ef77b6e925dab9cafea60f88d
Miguel Esteban <miguelestebn@gmail.com>
2026-08-12 17:45:49 +0200

    as

--- 002_Arrays/arrays.php
+++ 002_Arrays/arrays.php
@@ -9,7 +9,7 @@ $ciudad = "Gijón";
 $nacimiento = 1983;
 
 $actualYear = date('Y');
-$edad= $actualYear - $nacimiento
+$edad= $actualYear - $nacimiento;
 
 echo "Hola, mi nombre es $nombre y soy de $ciudad y tengo $edad años.";
 
@@ -30,7 +30,7 @@ echo "Mis películas favoritas son:";
 echo "<ul>";
 
 for($i=0; $i<count($pelis); $i++){
-    echo "<li> $pelis[i] </li>";
+    echo "<li> $pelis[$i] </li>";
 }
 echo "</ul>";
 
@@ -38,10 +38,10 @@ echo "</ul>";
 
 # Ejemplo con Aleatorio
 
-$cantidadPelis = count($pelis); // muestra la cantidad de elementos que tiene el array peliculas
-$aleatorio = rand(1,$cantidadPelis); // devuelve un valor aleatorio entre 1 y cantidad total de peliculas
+$cantidadPelis = count($pelis)-1; // muestra la cantidad de elementos que tiene el array peliculas (-1: arrays va de 0 a 4 si son 5 elementos)
+$aleatorio = rand(0,$cantidadPelis); // devuelve un valor aleatorio entre 0 y cantidad total de peliculas
 
-echo "hoy me apetece ver: $pelis[$aleatorio]"
+echo "hoy me apetece ver: $pelis[$aleatorio]";
 
 
 // - - - - - - - - - - - - - - - - - - - 
@@ -50,32 +50,49 @@ echo "hoy me apetece ver: $pelis[$aleatorio]"
 // Es un array dentro de un array permitiendo bolcar más datos. En cierto modo funciona como una cuadrícula de datos de excel
 
 $peliculas =[
-//   Título de la película      Director            Año   Ruta a Poster 
-    ["titulo"=> "El Imperio contraataca",  "director"=>"Irvin Kesner",     "anio"=>1980, "poster"=>"empirestrikesback.jpg"],
-    ["titulo"=> "Origen"                ,  "director"=>"Christoper Nolan", "anio"=>2011, "poster"=>"inception.jpg" ],
-    ["titulo"=> "Parque Jurásico",         "director"=>"Steven Spielberg", "anio"=>1992, "poster"=>"jurasicpark.jpg"],
-    ["titulo"=> "Regreso al Futuro",       "director"=>"Robert Zemedkins", "anio"=>1982, "poster"=>"backtothefuture.jpg"],
-    ["titulo"=> "La última cruzada",       "director"=>"Steven Spielberg", "anio"=>1988, "poster"=>"indiana.jpg"]
+//  0.Título                    1.Autor / director  2.Año   3.Ruta a Imagen 
+    ["El Imperio contraataca",  "Irvin Kesner",     1980,   "empirestrikesback.jpg"],
+    ["Origen"                ,  "Christoper Nolan", 2011,   "inception.jpg" ],
+    ["Parque Jurásico",         "Steven Spielberg", 1992,   "jurasicpark.jpg"],
+    ["Regreso al Futuro",       "Robert Zemedkins", 1982,   "backtothefuture.jpg"],
+    ["La última cruzada",       "Steven Spielberg", 1988,   "indiana.jpg"]
 ];
 
 // vamos a desplegar el array:
 foreach ($peliculas as $pelicula) {
     echo "<li>";
-    echo "<h2>$pelicula['titulo']</h2>";
-    echo "<p>Dirigido por: $pelicula['director']</p>";
-    echo "<p>Año: $pelicula['anio']</p>";
-    echo "<img src="$pelicula['poster']" alt="$pelicula['titulo']"</p>";
+    echo "<h2>".$pelicula[0]."</h2>";
+    echo "<p>Dirigido por: ".$pelicula[1]."</p>";
+    echo "<p>Año: ".$pelicula[2]."</p>";
+    echo "<img src='".$pelicula[3]."' alt='".$pelicula[0]."'>";
     echo "</li>";
 }
 
 // Otra manera de hacerlo:
-foreach ($peliculas as [$titulo, $director, $anio, $poster]) {
+foreach ($peliculas as [$titulo, $autor, $anio, $imagen]) {
     echo "<li>";
     echo "<h2>$titulo</h2>";
-    echo "<p>Dirigido por: $director</p>";
-    echo "<p>Año: $anio/p>";
-    echo "<img src="$poster" alt="$titulo"</p>";
+    echo "<p>Dirigido por: $autor</p>";
+    echo "<p>Año: $anio</p>";
+    echo "<img src='$imagen' alt='$titulo'>";
     echo "</li>";
+}
+
+
+# Funciones
+// podemos empaquetar instrucciones para reutilizarlas:
+
+function desplegar($array){
+    echo '<ul class="lista">'
+    foreach ($peliculas as [$titulo, $autor, $anio, $imagen]) {
+    echo '<li>';
+    echo '<h2>$titulo</h2>';
+    echo '<p>Dirigido por: $autor</p>';
+    echo '<p>Año: $anio</p>";
+    echo '<img src='$imagen' alt='$titulo'>";
+    echo '</li>";
+}
+
 }
 
 ?>
